ca7575ffcfecc5377fd3465f6eeb259f50e615bf
empireofryan <rjohnson2001@gmail.com>
2016-05-08 19:24:04 -0400

    angular app and controllers

---
+++ app/assets/javascripts/angular/controllers/controllers.js
@@ -0,0 +1,4 @@
+angular.module('controllers', [
+// controller names go here
+'ui.router', 'ngResource', 'templates', 'Devise', 'ui.bootstrap', 'ngMessages', 'ngFlash', 'truncate', 'ngFileUpload', 'App.filters'
+]);
