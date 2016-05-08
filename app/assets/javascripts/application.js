5c8980634faffeb1a96969eb2d5e7433286f90c3
empireofryan <rjohnson2001@gmail.com>
2016-05-08 18:39:29 -0400

    application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,13 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-devise
+//= require angular-rails-templates
+//= require angular-ui-router
+//= require angular-resource
+//= require angular-bootstrap/ui-bootstrap
+//= require angular-truncate-2
+//= require angular-messages
+//= require angular-flash-alert
+//= require ng-file-upload-shim
+//= require ng-file-upload
+//= require ngGallery
 //= require_tree .
