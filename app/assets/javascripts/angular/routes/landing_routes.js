0689aec7ddc824a572b4404091f37d4b47e87d6c
empireofryan <rjohnson2001@gmail.com>
2016-05-11 00:59:23 -0400

    refactor routes

--- app/assets/javascripts/angular/routes/landing_routes.js
+++ app/assets/javascripts/angular/routes/landing_routes.js
@@ -2,18 +2,36 @@ angular.module('app.routes.landing', [])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('app', {
+        abstract: true,
+        template: '<div ui-view></div>'
+      })
+      .state('app.landing', {
          url: '',
          controller: function () {},
          templateUrl: 'landing.html'
       })
-      .state('login', {
+      .state('app.login', {
         url: '/login',
         controller: 'LoginController',
         templateUrl: 'auth/login.html'
       })
-      .state('registration', {
+      .state('app.registration', {
         url: '/registration',
         controller: 'RegistrationController',
         templateUrl: 'auth/registration.html'
+      })
+      .state('app.scripts', {
+        url: '/scripts',
+        abstract: true
+      })
+      .state('app.scripts.index', {
+         url: '',
+         controller: 'ScriptsIndexController',
+         templateUrl: 'scripts/index.html',
+         resolve: {
+          scripts: function ($q) {
+            return [];
+          }
+         }
       })
   });
